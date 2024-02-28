1f03f94bac1ce2b79bbfb61264e303692522a6ee
imorenoca <imorenoca@outlook.com>
2024-02-28 13:31:22 +0100

    Creación de una consulta personalizada en PersonRepository, siguiendo las convenciones de nombre

--- springboot-jpa/src/main/java/com/andres/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
+++ springboot-jpa/src/main/java/com/andres/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
@@ -19,7 +19,9 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		List<Person> persons = (List<Person>) repository.findAll();
+		//List<Person> persons = (List<Person>) repository.findAll();
+
+		List<Person> persons = (List<Person>) repository.findByProgrammingLanguage("Ruby");
 
 		persons.stream().forEach(person -> {
 			System.out.println(person);
